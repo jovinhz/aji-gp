940a3c31a80de45ea3eb678fe4b1b40aed00f531
jovinhz <jovinharris@gmail.com>
2023-03-12 23:42:06 +0700

    update

---
+++ assets/index-5c178032.js
@@ -0,0 +1,1 @@
+import{F as t}from"./Form-d1b3d589.js";import{u as n,F as m}from"./Input-171d1ec7.js";t.useInjectFormItemContext=n;t.ItemRest=m;t.install=function(e){return e.component(t.name,t),e.component(t.Item.name,t.Item),e.component(m.name,m),e};

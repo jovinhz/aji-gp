ddedec1ecf28cac685d209ff8fe9ec006a85eb41
jovinhz <jovinharris@gmail.com>
2023-03-13 23:00:59 +0700

    update

---
+++ dist/assets/index-68047142.js
@@ -0,0 +1,1 @@
+import{F as t}from"./Form-bf91ef97.js";import{u as n,F as m}from"./Input-0690b7a8.js";t.useInjectFormItemContext=n;t.ItemRest=m;t.install=function(e){return e.component(t.name,t),e.component(t.Item.name,t.Item),e.component(m.name,m),e};
---
+++ dist/assets/index-87c6ef5a.js
@@ -0,0 +1,1 @@
+import{T as n,_ as e}from"./TabPane-e550f0d2.js";n.TabPane=e;n.install=function(o){return o.component(n.name,n),o.component(e.name,e),o};
